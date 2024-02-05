6bad5d7a3ba70238613598ca84cd5ed35c7902ab
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 16:14:22 +0500

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,5 +1,7 @@
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-   
+        if(year%4==0){
+            System.out.println("leap");
+        }else System.out.println("not leap");
     }
-    }
+ }
