c4d83f637718350ef932ded6aa09420f6112bd6c
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 15:56:03 +0500

    Update SwitchDaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -4,32 +4,32 @@ public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
         switch(month):
             case (1){
-                System.out.println{31}
+                System.out.println{31};
             }
              case (1){
-                System.out.println{31}
+                System.out.println{31};
             } case (2){
-                System.out.println{28}
+                System.out.println{28};
             } case (3){
-                System.out.println{31}
+                System.out.println{31};
             } case (4){
-                System.out.println{30}
+                System.out.println{30};
             } case (5){
-                System.out.println{31}
+                System.out.println{31};
             } case (6){
-                System.out.println{30}
+                System.out.println{30};
             } case (7){
-                System.out.println{31}
+                System.out.println{31};
             } case (8){
-                System.out.println{30}
+                System.out.println{30};
             } case (9){
-                System.out.println{30}
+                System.out.println{30};
             } case (10){
-                System.out.println{31}
+                System.out.println{31};
             } case (11){
-                System.out.println{30}
+                System.out.println{30};
             } case (12){
-                System.out.println{31}
+                System.out.println{31};
             }
     }
 }
